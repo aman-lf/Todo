240cf0aae8cb0067f68d29f5fdad1c5d4ed172cb
aman-lf <amanshrestha@lftechnologies.com>
2022-12-07 17:57:33 +0545

    Added add function

--- main.go
+++ main.go
@@ -49,6 +49,18 @@ func getAllTodos() []interface{} {
 	return results
 }
 
+func insertTodo(item string, completed bool) {
+	client := InitDataLayer()
+	coll := client.Database("todoTraining").Collection("todos")
+
+	todo := bson.D{{"item", item}, {"completed", completed}}
+	_, err := coll.InsertOne(ctx, todo)
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+}
+
 func indexHandler(response http.ResponseWriter, request *http.Request) {
 	if request.Method != "GET" {
 		http.Error(response, "Method is not supported.", http.StatusNotFound)
@@ -75,9 +87,52 @@ func indexHandler(response http.ResponseWriter, request *http.Request) {
 	}
 }
 
+func addHandler(response http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" && request.Method != "POST" {
+		http.Error(response, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	if request.Method == "GET" {
+		files := []string{
+			"static/base.gohtml",
+			"static/add.gohtml",
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			log.Print(err.Error())
+			http.Error(response, "Internal Server Error", 500)
+			return
+		}
+
+		err = ts.ExecuteTemplate(response, "base", nil)
+		if err != nil {
+			log.Print(err.Error())
+			http.Error(response, "Internal Server Error", 500)
+		}
+	} else if request.Method == "POST" {
+		if err := request.ParseForm(); err != nil {
+			log.Print(err.Error())
+			return
+		}
+		item := request.FormValue("item")
+		completed_form := request.FormValue("completed")
+		var completed bool
+		if completed_form == "true" {
+			completed = true
+		} else {
+			completed = false
+		}
+
+		insertTodo(item, completed)
+		fmt.Fprintf(response, "Data inserted successfully")
+	}
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/add", indexHandler)
+	http.HandleFunc("/add", addHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 
